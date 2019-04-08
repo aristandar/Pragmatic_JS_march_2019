1bd1bb7d0697753de71b6d17c6847a3fd19d65c7
aristandar <48587453+aristandar@users.noreply.github.com>
2019-04-08 17:27:37 +0300

    homework1

--- homework 1/numbers.js
+++ homework 1/numbers.js
@@ -1,6 +1,4 @@
 function printRandNum() {
-  let num = Math.round(
-    Math.random() * 100);
-  document.body.innerHTML +=
-    `<div>${num}</div>`;
+  let num = Math.round(Math.random() * 100);
+  document.body.innerHTML += `<div>${num}</div>`;
 }
