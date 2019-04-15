2d09a88391a980fae5bb0550a2a5b9e4357f8687
aristandar <48587453+aristandar@users.noreply.github.com>
2019-04-15 18:01:58 +0300

    homework2-task5

---
+++ homework 2/script.js
@@ -0,0 +1,35 @@
+function minMaxSum() {
+
+    // Get the user input values
+    let a0 = Number(document.getElementsByClassName('input-task-5')[0].value);
+    let a1 = Number(document.getElementsByClassName('input-task-5')[1].value);
+    let a2 = Number(document.getElementsByClassName('input-task-5')[2].value);
+    let a3 = Number(document.getElementsByClassName('input-task-5')[3].value);
+    let a4 = Number(document.getElementsByClassName('input-task-5')[4].value);
+
+    let array = [a0, a1, a2, a3, a4];
+
+
+  function isPositiveInteger() {
+    console.log(array);
+    let status = true;
+    for (var i = 0; i < array.length; i++) {
+      if (Number.isInteger(array[i]) && array[i] > Number(0)) {
+      status = true;    
+      }
+      else {
+        console.log(array[i]);
+        status = false;
+        break;
+      }
+      return status;
+    }
+  }
+
+if (isPositiveInteger()) {
+console.log("positive");
+}
+  else {
+  document.getElementById('results-five').innerHTML = 'Please enter positive integer numbers!';;
+  }
+}
