a94591e0d6918c15eb61ad1dec3b2a27d8bc6956
aristandar <48587453+aristandar@users.noreply.github.com>
2019-04-17 17:37:47 +0300

    vsc

--- homework 2/script.js
+++ homework 2/script.js
@@ -1,13 +1,13 @@
 function isPositiveInteger(num) {
-    let status = true;
-      if (Number.isInteger(num) && Number(num) > Number(0)) {
-      status = true;    
-      }
-      else {
-        status = false;
-      }
-      return status;
-    }
+  let status = true;
+  if (Number.isInteger(num) && Number(num) > Number(0)) {
+    status = true;
+  }
+  else {
+    status = false;
+  }
+  return status;
+}
 
 
 //Problem 3: A Very Big Sum
@@ -16,52 +16,52 @@ function veryBigSum() {
 
   // read the size of the array
   let arrNumber = Number(document.getElementsByClassName('input-task-3')[0].value);
-  if ( ! isPositiveInteger(arrNumber)) {
+  if (!isPositiveInteger(arrNumber)) {
     document.getElementById('results-three').innerHTML = 'Result: Please enter positive integer!!!';
   }
   else {
-  let arr = [];
+    let arr = [];
 
-  //read the array from prompt
-  for (var i = 0; i < arrNumber; i++) {
-    arr[i] = Number(prompt("Моля въведете число:"));
+    //read the array from prompt
+    for (var i = 0; i < arrNumber; i++) {
+      arr[i] = Number(prompt("Моля въведете число:"));
+    }
+
+    console.log(arr);
+
+    bigSum = 0;
+    for (var i = 0; i < arrNumber; i++) {
+      bigSum += arr[i];
+    }
+
+    document.getElementById('results-three').innerHTML = 'Result: the integer sum of the elements in the array = ' + bigSum;
   }
-
-  console.log(arr);
-
-  bigSum = 0;
-  for (var i = 0; i < arrNumber; i++) {
-    bigSum +=arr[i];
-  }
-
-  document.getElementById('results-three').innerHTML = 'Result: the integer sum of the elements in the array = ' + bigSum;
-}
 }
 
 
 function minMaxSum() {
 
-    // Get the user input values
-    let a0 = Number(document.getElementsByClassName('input-task-5')[0].value);
-    let a1 = Number(document.getElementsByClassName('input-task-5')[1].value);
-    let a2 = Number(document.getElementsByClassName('input-task-5')[2].value);
-    let a3 = Number(document.getElementsByClassName('input-task-5')[3].value);
-    let a4 = Number(document.getElementsByClassName('input-task-5')[4].value);
+  // Get the user input values
+  let a0 = Number(document.getElementsByClassName('input-task-5')[0].value);
+  let a1 = Number(document.getElementsByClassName('input-task-5')[1].value);
+  let a2 = Number(document.getElementsByClassName('input-task-5')[2].value);
+  let a3 = Number(document.getElementsByClassName('input-task-5')[3].value);
+  let a4 = Number(document.getElementsByClassName('input-task-5')[4].value);
 
-    let array = [a0, a1, a2, a3, a4];
+  let array = [a0, a1, a2, a3, a4];
 
-if ((isPositiveInteger(a0)) && (isPositiveInteger(a1)) && (isPositiveInteger(a2)) && (isPositiveInteger(a3)) && (isPositiveInteger(a4))) {
-console.log("positive");
-let sortArray = array.sort((a, b) => a - b);
-console.log(sortArray);
-let min = sortArray[0] + sortArray[1] + sortArray[2] + sortArray[3];
-let max = sortArray[1] + sortArray[2] + sortArray[3] + sortArray[4];
-document.getElementById('results-five').innerHTML = 'Result: (min Sum , max Sum) = ' + min + ' ' + max;
-}
+  if ((isPositiveInteger(a0)) && (isPositiveInteger(a1)) && (isPositiveInteger(a2)) && (isPositiveInteger(a3)) && (isPositiveInteger(a4))) {
+    console.log("positive");
+    let sortArray = array.sort((a, b) => a - b);
+    console.log(sortArray);
+    let min = sortArray[0] + sortArray[1] + sortArray[2] + sortArray[3];
+    let max = sortArray[1] + sortArray[2] + sortArray[3] + sortArray[4];
+    document.getElementById('results-five').innerHTML = 'Result: (min Sum , max Sum) = ' + min + ' ' + max;
+  }
 
   else {
-  document.getElementById('results-five').innerHTML = 'Please enter positive integer numbers!';
-  } 
+    document.getElementById('results-five').innerHTML = 'Please enter positive integer numbers!';
+  }
 }
 
 
